4371ca581df30ebd3a8aab161933d49d03d88e1a
Kirill Lepikhin <lepihin.k@gmail.com>
2019-10-03 18:43:52 +0800

    Fixed panic when all goroutines are sleep

--- goemitter_test.go
+++ goemitter_test.go
@@ -1,7 +1,10 @@
 package Emitter
 
 import (
+	"math/rand"
 	"reflect"
+	"sync"
+	"sync/atomic"
 	"testing"
 )
 
@@ -42,6 +45,63 @@ func TestOnce(t *testing.T) {
 	emitter.EmitSync("testevent")
 
 	expect(t, 1, counter)
+}
+
+func TestRandomConcurrentCalls(t *testing.T) {
+	emitter := Construct()
+
+	var counter int32
+	var err error
+
+	randomCallsFn := func() {
+		defer func() {
+			if r := recover(); r != nil {
+				err = r.(error)
+			}
+		}()
+
+		fn1 := func(args ...interface{}) {
+			atomic.AddInt32(&counter, 1)
+		}
+		fn2 := func(args ...interface{}) {
+			atomic.AddInt32(&counter, 1)
+		}
+
+		events := []string{"event1", "event2", "event3"}
+		fns := []func(...interface{}){fn1, fn2}
+
+		m := map[int]interface{}{}
+		for i := 0; i < 100; i++ {
+			eventIdx := int(rand.Int31()) % len(events)
+			fnIdx := int(rand.Int31()) % len(fns)
+			key := fnIdx<<4 + eventIdx
+
+			action := int(rand.Int31())
+			if action%3 == 0 {
+				if _, ok := m[key]; !ok {
+					emitter.On(events[eventIdx], fns[fnIdx])
+					m[key] = nil
+				}
+			} else if action%7 == 0 {
+				emitter.RemoveListener(events[eventIdx], fns[fnIdx])
+				delete(m, key)
+			} else {
+				emitter.EmitAsync(events[eventIdx], nil)
+			}
+		}
+	}
+
+	wg := sync.WaitGroup{}
+	for j := 0; j < 10; j++ {
+		go func() {
+			wg.Add(1)
+			randomCallsFn()
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+
+	expect(t, nil, err)
 }
 
 func expect(t *testing.T, a interface{}, b interface{}) {
